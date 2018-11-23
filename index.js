4246c4c6a4a6a23ef3084a3b4b6f61632c9eece6
Dafna Ehrman <dafna972@gmail.com>
2018-11-23 11:53:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const async=require('async')
-const fetch = require("node-fetch")
+var request = require("request");
 
 const app = express()
 const port = 3000
@@ -8,43 +8,68 @@ const noDependenciesString = "no dependencies"
 const errorNpmString = "Not Found"
   
 let pckgsDic = {}
-app.listen(port, () => console.log(`Dependencies service is listening on port ${port}! \nUsing example: 127.0.0.1:3000/express`))
+app.listen(port, () => console.log(`Dependencies service is listening on port ${port}! \nUsing example: 127.0.0.1:${port}/express`))
 
-app.get('/:package', function (req, res) {
-    
-    if (!pckgsDic[req.params.package]){        
-        GetNpmDependencies(req.params.package)
-            .then(res.json(RootTree(req.params.package)));
-    }
+app.get('/:package', async (req, res) => {
 
-     else 
+    if (pckgsDic[req.params.package])
     {
+        res.json(pckgsDic[req.params.package])
         console.log("cached")
-        res.json(RootTree(req.params.package))
     }
-        
+    else
+        GetNpmDependencies(req.params.package)
+            .then(function(x) {
+                res.json(RootTree(req.params.package))
+                console.log('works')
+            }).catch(function(err) {
+                console.log(err.stack)
+            })        
 })
 
 function GetNpmDependencies(package) {
-    return fetch(`https://registry.npmjs.org/${package}/latest`)
-        .then(x => x.json())
-        .then(x=> {
-            if (x.dependencies)
-            {                
-                pckgsDic[package] = x.dependencies
-                for(var propertyName in x.dependencies) {
-                    if (!pckgsDic[package])
-                        GetNpmDependencies(propertyName)}
+    // Setting URL and headers for request
+    var options = {
+        url: `https://registry.npmjs.org/${package}/latest`,
+        headers: {
+            'User-Agent': 'request'}
+      };
+    
+    // Return new promise 
+    return new Promise(function(resolve, reject) {
+        request.get(options, function(err, resp, package) {
+            if (err) {
+                reject(err);
+            } else {
+                json = JSON.parse(package)            
+                if (json.dependencies)
+                {
+                    pckgsDic[json.name] = json.dependencies
+                    array = []
+                    for(var propertyName in json.dependencies) {
+                        if (!pckgsDic[propertyName])
+                            array.concat(GetNpmDependencies(propertyName))}
+                    resolve(Promise.all(array))
+                }
+
+                else 
+                {
+                    pckgsDic[json.name] = noDependenciesString
+                    resolve(json)
+                }
             }
-            
-            else    
-                pckgsDic[package] = noDependenciesString
-            })
+        })
+    })
+}
+
+
+var errHandler = function(err) {
+    console.log(err);
 }
 
 function RootTree(package){
-    let result = {}
-    
+    let result = {} 
+
     result[package] = pckgsDic[package]
 
     if (result[package] == noDependenciesString)
@@ -55,3 +80,4 @@ function RootTree(package){
 
     return result[package]
 }
+
